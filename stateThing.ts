f1bc36090db72416e64ba31e8d04dfc3c1f74e2f
Brian Prost <me@brianprost.com>
2023-07-03 19:47:43 -0400

    hell yeah it's writing just those

--- stateThing.ts
+++ stateThing.ts
@@ -1,10 +1,10 @@
-import { Client, Collection, GatewayIntentBits, Message, TextChannel } from "discord.js";
+import { Client, GatewayIntentBits, Message, TextChannel } from "discord.js";
 import { getDatabase, ref, get, set } from "firebase/database";
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { config } from "dotenv";
 import { initializeApp } from "firebase/app";
+// import { collection, doc, getDoc, getDocs, getFirestore, query, setDoc, where } from "firebase/firestore";
 config();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -23,6 +23,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
+// const firestore = getFirestore(firebaseApp);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
@@ -35,36 +36,81 @@ client.once("ready", () => {
 const channelId = process.env.DISCORD_CHANNEL_ID;
 
 client.once("ready", async () => {
-  setInterval(async () => {
+  // set(ref(database, `discordState/${process.env.DISCORD_CHANNEL_NAME}`), {});
+  // setInterval(async () => {
+  console.log("Checking for messages");
 
-    console.log("Checking for messages")
+  const channel: TextChannel = client.channels.cache.get(
+    channelId!
+  ) as TextChannel;
 
-    const channel: TextChannel = client.channels.cache.get(channelId!) as TextChannel;
+  const allMessages = await channel!.messages.fetch();
+  const filteredMessages = allMessages.filter((message) => {
+    const inPrevious360Days =
+      new Date(message.createdTimestamp) >
+      new Date(new Date().setDate(new Date().getDate() - 500));
+    return inPrevious360Days && message.attachments.size > 0;
+  });
 
-    const allMessages = await channel!.messages.fetch();
-    const filteredMessages = allMessages.filter((message) => {
-      const inPrevious360Days = new Date(message.createdTimestamp) > new Date(new Date().setDate(new Date().getDate() - 500));
-      return inPrevious360Days && message.attachments.size > 0;
+  const previousStateRef = ref(
+    database,
+    `discordState/${process.env.DISCORD_CHANNEL_NAME}`
+  );
+  const previousStateSnapshot = await get(previousStateRef);
+  const previousState = previousStateSnapshot.val();
+  const previousStateIDs = Object.keys(previousState);
+
+  const newMessages = filteredMessages.filter(
+    (message) => !previousStateIDs.includes(message.id)
+  );
+  if (newMessages.size > 0) {
+    newMessages.map(async (message) => {
+      const { id } = message;
+      try {
+        const actualMessage = await channel.messages.fetch(id);
+        const userWhoPosted = actualMessage.author.username;
+        const attachmentUrl = actualMessage.attachments.first()?.url;
+        const attachmentName = actualMessage.attachments.first()?.name;
+        const reactors = actualMessage.reactions.cache.each(async (reaction) => {
+          const reactors = await reaction.users.fetch();
+          reactors.forEach((user) => console.log(user.username));
+          return reactors.map(user => user.username);
+        })
+        await set(ref(database, `discordState/${process.env.DISCORD_CHANNEL_NAME}/${id}`), {
+          userWhoPosted,
+          attachmentUrl,
+          attachmentName,
+          reactors
+        });
+      } catch (err) {
+        console.log(err);
+      }
     });
 
-    const previousStateRef = ref(database, `discordState/${process.env.DISCORD_CHANNEL_NAME}`);
-    const previousStateSnapshot = await get(previousStateRef);
-    const previousState = previousStateSnapshot.val();
-    const previousStateIDs = Object.keys(previousState);
-
-    const newMessages = filteredMessages.filter((message) => !previousStateIDs.includes(message.id));
-    if (newMessages.size > 0) {
-      console.log("New messages found")
-      const newMessagesJSON = newMessages.map((message) => {
-        return {
-          [message.id]: message.toJSON(),
-        };
-      });
-      newMessagesJSON.forEach((message) => {
-        set(ref(database, `discordState/${process.env.DISCORD_CHANNEL_NAME}/${Object.keys(message)[0]}`), message[Object.keys(message)[0]]);
-      });
-    }
-  }, 1000);
+  }
+  // }, 1000);
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+// function areAnyInFirestore(filteredMessages: Collection<string, Message<true>>) {
+//   firebase
+//   const channelStateRef = collection(firestore, `${process.env.DISCORD_CHANNEL_NAME}`);
+
+//   console.log(`There are ${filteredMessages.size} messages to check`)
+
+//   const nonDbMessages = filteredMessages.filter(async (message) => {
+//     console.log(`Checking if ${message.id} is in firestore`)
+//     const isInFirestore = query(channelStateRef, where("id", "==", message.id));
+//     const isInFirestoreSnapshot = await getDocs(isInFirestore);
+//     const isInFirestoreData = isInFirestoreSnapshot.docs.map((doc) => doc.data());
+//     return isInFirestoreData.length !== 0;
+//   });
+//   console.log("size: ", nonDbMessages.size)
+
+//   // add to firestore
+//   nonDbMessages.forEach(async (message) => {
+//     console.log("Adding message to firestore")
+//     await setDoc(doc(channelStateRef, message.id), message.toJSON());
+//   });
+// }
