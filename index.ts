69267a53f981e0c39f23980335fc05ce62aa53d0
Brian Prost <me@brianprost.com>
2023-07-03 17:16:46 -0400

    good enough for now

--- index.ts
+++ index.ts
@@ -23,6 +23,7 @@ const discordClient = new Client({
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREABSE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
@@ -70,7 +71,7 @@ function botLog(message: string) {
   console.log(`🤖 ${message}`);
 }
 
-async function promoteItOnAbrys(
+export async function promoteItOnAbrys(
   url: string,
   discordUser: string,
   postHash: string
--- stateThing.ts
+++ stateThing.ts
@@ -1,73 +1,70 @@
 import { Client, Collection, GatewayIntentBits, Message, TextChannel } from "discord.js";
+import { getDatabase, ref, get, set } from "firebase/database";
 import fs from "fs";
-import crypto from "crypto";
 import path from "path";
 import { fileURLToPath } from "url";
 import { config } from "dotenv";
-import { DiscordMessage } from "./types/DiscordMessage";
+import { initializeApp } from "firebase/app";
 config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREABSE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
 });
 
 client.once("ready", () => {
-  console.log("Client is ready");
-  // create (and overwrite) the log.json file
-  // fs.writeFile(path.join(__dirname, "log.json"), "[]", (err) => {
-  //   if (err) console.error(`Error writing to log: ${err.message}`);
-  // });
+  console.log("🧽 I'm ready!");
 });
 
-// Replace with your channel ID
 const channelId = process.env.DISCORD_CHANNEL_ID;
-const dateToCheck = new Date().toISOString().slice(0, 10);
 
-async function loadLog() {
-  // load the log from the file
-  fs.readFile(path.join(__dirname, "log.json"), (err, data) => {
-    if (err) console.error(`Error reading log: ${err.message}`);
-    return JSON.parse(data as unknown as string);
-  });
-}
-
-async function writeToLog(state: Collection<string, Message<true>>) {
-  fs.writeFile(
-    path.join(__dirname, "log.json"),
-    JSON.stringify(state),
-    (err) => {
-      if (err) console.error(`Error writing to log: ${err.message}`);
-    }
-  );
-}
-
-client.once("ready", () => {
+client.once("ready", async () => {
   setInterval(async () => {
+
     console.log("Checking for messages")
+
     const channel: TextChannel = client.channels.cache.get(channelId!) as TextChannel;
+
     const allMessages = await channel!.messages.fetch();
     const filteredMessages = allMessages.filter((message) => {
-      const messageDate = message.createdAt.toISOString().slice(0, 10);
-      return messageDate === dateToCheck && message.attachments.size > 0;
+      const inPrevious360Days = new Date(message.createdTimestamp) > new Date(new Date().setDate(new Date().getDate() - 500));
+      return inPrevious360Days && message.attachments.size > 0;
     });
-    await writeToLog(filteredMessages);
+
+    const previousStateRef = ref(database, `discordState/${process.env.DISCORD_CHANNEL_NAME}`);
+    const previousStateSnapshot = await get(previousStateRef);
+    const previousState = previousStateSnapshot.val();
+    const previousStateIDs = Object.keys(previousState);
+
+    const newMessages = filteredMessages.filter((message) => !previousStateIDs.includes(message.id));
+    if (newMessages.size > 0) {
+      console.log("New messages found")
+      const newMessagesJSON = newMessages.map((message) => {
+        return {
+          [message.id]: message.toJSON(),
+        };
+      });
+      newMessagesJSON.forEach((message) => {
+        set(ref(database, `discordState/${process.env.DISCORD_CHANNEL_NAME}/${Object.keys(message)[0]}`), message[Object.keys(message)[0]]);
+      });
+    }
   }, 1000);
 });
-
-// async function getHash(string) {
-//   const encoder = new TextEncoder();
-//   const data = encoder.encode(string);
-//   const hash = await crypto.subtle.digest("SHA-256", data);
-//   return hash;
-// }
-
-//   // log the content of the messages from the specified date
-//   messagesOnDate.forEach((message) => {
-//     console.log(`${message.author.username}: ${message.content}`);
-//   });
-// console.log(await stateOfTheChannel);
 
 client.login(process.env.DISCORD_TOKEN);
