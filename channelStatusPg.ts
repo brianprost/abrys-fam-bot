d9429ad2e46f31697d7ad91e11166fe88616249e
Brian Prost <me@brianprost.com>
2023-07-04 17:18:23 -0400

    Add bot_local and channel_status services to docker-compose.yml

--- channelStatusPg.ts
+++ channelStatusPg.ts
@@ -1,43 +1,39 @@
 import { Client, GatewayIntentBits, TextChannel } from "discord.js";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { foreignKey, integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-import { InferModel } from "drizzle-orm";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { InferModel, isNull } from "drizzle-orm";
 import pg from "pg";
 const { Pool } = pg;
 config();
 
+const APPROVED_USERS = [
+  "angular emoji",
+  "angularemoji",
+  "angular emoji#6001",
+  "luluwav",
+  "lulu.wav",
+  "luluwav#5414",
+  "sleeprides",
+];
+
 export type Promotion = InferModel<typeof promotions>;
 
 const pool = new Pool({
-  connectionString: process.env.PG_DATABASE_CONNECTION_STRING
-})
+  connectionString: process.env.PG_DATABASE_CONNECTION_STRING,
+});
 
 export const promotions = pgTable("promotions", {
   id: serial("id").primaryKey(),
   userWhoPosted: text("user_who_posted").notNull(),
   messageId: text("message_id").notNull(),
   attachmentUrl: text("attachment_url"),
-  attachmentName: text("attachment_name"),
   instagramUrl: text("instagram_url"),
-  createdAt: timestamp("created_at").defaultNow().notNull()
+  notes: text("notes"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-export const reactions = pgTable("reactions", {
-  id: serial("id").primaryKey(),
-  reactorUsername: text("reactor_username").notNull(),
-  messageId: text("message_id").notNull(),
-  promotionId: integer("promotion_id").notNull(),
-}, (reactions) => {
-  return {
-    promotionIdFk: foreignKey({
-      columns: [reactions.promotionId],
-      foreignColumns: [promotions.id],
-    })
-  }
-});
-
-const db = drizzle(pool)
+const db = drizzle(pool);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
@@ -50,59 +46,79 @@ client.once("ready", () => {
 const channelId = process.env.DISCORD_CHANNEL_ID;
 
 client.once("ready", async () => {
-  console.log("Checking for messages");
+  console.log("👀 Checking for messages");
 
   const channel: TextChannel = client.channels.cache.get(
     channelId!
   ) as TextChannel;
 
-  const allMessages = await channel!.messages.fetch();
-  const filteredMessages = allMessages.filter((message) => {
+  const allMessages = await channel!.messages.fetch({ limit: 100 });
+
+  const recentMessages = allMessages.filter((message) => {
     const inPrevious360Days =
       new Date(message.createdTimestamp) >
       new Date(new Date().setDate(new Date().getDate() - 500));
     return inPrevious360Days && message.attachments.size > 0;
   });
 
-  allMessages.map(async (message) => {
+  const previousPromotionsIds = (
+    await db.select({ messageId: promotions.messageId }).from(promotions)
+  ).map((message) => message.messageId);
+
+  const newMessageIds = recentMessages
+    .map((message) => message.id)
+    .filter((id) => !previousPromotionsIds.includes(id));
+  const newMessages = recentMessages.filter((message) =>
+    newMessageIds.includes(message.id)
+  );
+  const newMessagesForPossiblePromotion = newMessages.filter((message) => {
+    const hasAttachment = message.attachments.size > 0;
+    const hasReaction = message.reactions.cache.size > 0;
+    return hasAttachment && hasReaction;
+  });
+
+  console.log(
+    "newMessagesForPossiblePromotion",
+    newMessagesForPossiblePromotion.size
+  );
+
+  newMessagesForPossiblePromotion.map(async (message) => {
     const { id } = message;
     try {
       const actualMessage = await channel.messages.fetch(id);
       const userWhoPosted = actualMessage.author.username;
       const attachmentUrl = actualMessage.attachments.first()?.url;
-      const attachmentName = actualMessage.attachments.first()?.name;
-      const reactors = await Promise.all(actualMessage.reactions.cache.map(async (reaction) => {
-        const reactorsTmp = await reaction.users.fetch();
-        return reactorsTmp.map((user) => user.username);
-      }))
-      if (reactors.length > 0) {
-        // first let's add the promotion to the promotions table
+      const messageDate = new Date(actualMessage.createdTimestamp);
+      const reactors = await Promise.all(
+        actualMessage.reactions.cache.map(async (reaction) => {
+          const reactorsTmp = await reaction.users.fetch();
+          return reactorsTmp.map((user) => user.username);
+        })
+      );
+      const approvedReactors = reactors.map((reactor) => {
+        return reactor.filter((reactor) => APPROVED_USERS.includes(reactor));
+      });
+      if (reactors.length > 0 && approvedReactors.length > 0) {
         const messageId = actualMessage.id;
+        const note = `${userWhoPosted} promoted it on @abrys_fam. at ${messageDate}.`;
         const newDbRecord = {
           userWhoPosted,
           messageId,
           attachmentUrl,
-          attachmentName
-        }
-        const promotionInsertion = await db.insert(promotions).values(newDbRecord).returning();
-        const promotionId = promotionInsertion[0].id;
-        for (const reactorList of reactors) {
-          for (const reactor of reactorList) {
-            const newReactionRecord = {
-              reactorUsername: reactor,
-              messageId,
-              promotionId
-            };
-            await db.insert(reactions).values(newReactionRecord).returning();
-          }
-        }
+          notes: note,
+          createdAt: messageDate,
+        };
+        await db.insert(promotions).values(newDbRecord).returning();
       }
     } catch (err) {
       console.log(err);
     }
   });
-  const allPromotions = await db.select().from(promotions);
-  console.log(allPromotions);
+  const nonPromotedPromotions = await db
+    .select()
+    .from(promotions)
+    .where(isNull(promotions.instagramUrl));
+  console.log("nonPromotedPromotions", nonPromotedPromotions.length);
 });
 
 client.login(process.env.DISCORD_TOKEN);
